9d67461ab7da02b7754e55523de7202e3ee08cec
Wolfgang Zolleis <wzolleis@web.de>
2021-07-11 19:00:14 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+
+import * as React from 'react';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+interface Props {
+   name:
+    string
+}
+
+class App extends React.Component<Props> {
+  render() {
+    const { name } = this.props;
+    return (
+      <>
+        <h1>
+          Hello {name}
+        </h1>
+        <button type="button" class="btn btn-primary">
+          This is a bootstrap button
+        </button>
+      </>
+    );
+  }
+}
+
+export default App;
